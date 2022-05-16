15df58a091c40f5bbb629159cd87f87f51bdea24
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-05-16 10:57:16 -0700

    gitignore and modules/node intro

---
+++ Web/Back/js/modules_3/hello.js
@@ -0,0 +1,7 @@
+import {student}  from '../modules_3/student.js'
+
+
+const student_1 = new student('Andrew', 'Dunkerley', 'a@gmail.com')
+
+student_1.printInfo()
+
---
+++ Web/Back/js/modules_3/student.js
@@ -0,0 +1,21 @@
+class student
+{
+    constructor(name, surname, email)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    printInfo()
+    {
+        console.log(`Name:  ${this.name} ${this.surname} Email: ${this.email}`)
+
+    }
+
+
+}
+
+export {student}
+
+
